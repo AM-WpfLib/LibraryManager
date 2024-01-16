4bb7696136065c8bb14f67ceb4343bb4f7f9cf27
AbidMahmood <abid.mahmood@bluelake-it.de>
2024-01-16 19:31:12 +0100

    initial clean up

---
+++ CrossCutting/LibraryManager.Core/Common/PropertyChangedNotifier.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LibraryManager.Core.Common
+{
+    public abstract class PropertyChangedNotifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged(params string[] names)
+        {
+            OnPropertyChanged((IEnumerable<string>)names);
+        }
+
+        protected virtual void OnPropertyChanged(IEnumerable<string> names)
+        {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            if (propertyChanged == null)
+            {
+                return;
+            }
+
+            foreach (string name in names)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
